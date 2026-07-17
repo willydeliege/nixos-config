f9972c6065a4610a25c056970eec2bbaaece04b2
Frédérix Willem <frederic.willem@gmail.com>
2026-07-17 11:21:45 +0200

    clean flake.nix

--- flake.nix
+++ flake.nix
@@ -17,13 +17,11 @@
       home-manager,
     }:
     {
+      # TODO: a investiguer
+      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
       nixosConfigurations = {
-        # Remplace "nixos" par le hostname de ta machine s'il est différent
-        # Dans ton fichier, c'est : networking.hostName = "nixos";
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
