56fdc5a418e0ce5243239e7a1cca8d07e71bea5b
Frédérix Willem <frederic.willem@gmail.com>
2026-07-17 09:21:41 +0200

    add configuration and hardware nix files

--- flake.nix
+++ flake.nix
@@ -10,6 +10,16 @@
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations = {
+      # Remplace "nixos" par le hostname de ta machine s'il est différent
+      # Dans ton fichier, c'est : networking.hostName = "nixos";
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+        ];
+      };
+    };
   };
 }
