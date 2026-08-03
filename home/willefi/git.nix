c42ab260b1425777a5de1eb8f3c19577854e7a03
Frédérix Willem <frederic.willem@gmail.com>
2026-08-03 22:36:30 +0200

    update lazygit theme

--- home/willefi/git.nix
+++ home/willefi/git.nix
@@ -6,6 +6,20 @@
       git.pagers = [
         { pager = "delta --dark --paging=never"; }
       ];
+      gui = {
+        theme = {
+          activeBorderColor = [ "#63F2F1" ];
+          inactiveBorderColor = [ "#585273" ];
+          optionsTextColor = [ "#D4BFFF" ];
+          selectedLineBgColor = [ "#3E3859" ];
+          selectedRangeBgColor = [ "#2D2B40" ];
+          cherryPickedCommitBgColor = [ "#585273" ];
+          cherryPickedCommitFgColor = [ "#91DDFF" ];
+          unstagedChangesColor = [ "#F48FB1" ];
+          defaultFgColor = [ "#CBE3E7" ];
+          searchingActiveBorderColor = [ "#FFE6B3" ];
+        };
+      };
     };
   };
 
