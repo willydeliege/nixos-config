6f597d3e2b476dd5b024ff3126c3560f1172fc88
Frédérix Willem <frederic.willem@gmail.com>
2026-08-05 07:59:19 +0200

    clean code

--- home/willefi/dotfiles.nix
+++ home/willefi/dotfiles.nix
@@ -1,10 +1,5 @@
 { config, ... }:
 {
-
-  # wayland.windowManager.sway = {
-  #   enable = false;
-  #   config.terminal = "kitty";
-  # };
   xdg.configFile."sway".source =
     config.lib.file.mkOutOfStoreSymlink /home/willefi/nixos-config/home/willefi/config/sway;
 }
