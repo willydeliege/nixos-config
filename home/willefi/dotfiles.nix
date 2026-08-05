f558ae096d43b345b8c7fb6bd36014af770a78c7
Frédérix Willem <frederic.willem@gmail.com>
2026-08-05 08:04:20 +0200

    add swaync config

--- home/willefi/dotfiles.nix
+++ home/willefi/dotfiles.nix
@@ -2,4 +2,6 @@
 {
   xdg.configFile."sway".source =
     config.lib.file.mkOutOfStoreSymlink /home/willefi/nixos-config/home/willefi/config/sway;
+  xdg.configFile."swaync".source =
+    config.lib.file.mkOutOfStoreSymlink /home/willefi/nixos-config/home/willefi/config/swaync;
 }
