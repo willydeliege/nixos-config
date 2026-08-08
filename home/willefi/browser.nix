5499fecaf2b24c6c0752637a7f0ee4301433d1fe
Frédéric Willem <frederic.willem@gmail.com>
2026-08-08 22:37:08 +0200

    remove boolmark manager

--- home/willefi/browser.nix
+++ home/willefi/browser.nix
@@ -3,9 +3,6 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    buku # Bookmarks manager
-  ];
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
@@ -21,35 +18,4 @@
     ];
 
   };
-
-  # Configuration des services systemd utilisateur
-  systemd.user.services.sync-brave-buku = {
-    Unit = {
-      Description = "Synchronisation automatique des marque-pages Brave vers Buku";
-    };
-    Service = {
-      Type = "oneshot";
-      # Le script vérifie si le fichier existe avant d'importer pour éviter les erreurs au premier démarrage
-      ExecStart = "${pkgs.bash}/bin/bash -c 'if [ -f ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks ]; then ${pkgs.buku}/bin/buku -i ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks; fi'";
-    };
-  };
-
-  # Déclencheur (Timer) pour exécuter la synchronisation périodiquement
-  systemd.user.timers.sync-brave-buku = {
-    Unit = {
-      Description = "Déclencheur pour la synchronisation Brave-Buku";
-    };
-    Timer = {
-      # S'exécute 5 minutes après le démarrage de la session utilisateur
-      OnStartupSec = "5m";
-      # Puis se répète toutes les heures
-      OnUnitActiveSec = "1h";
-    };
-    Install = {
-      WantedBy = [ "timers.target" ];
-    };
-  };
-  # NOTE: ne pas oublier
-  # systemctl --user daemon-reload
-  # systemctl --user enable --now sync-brave-buku.timer
 }
